2851733554cc15b6d4abe824fc719fac359a0fb0
Davide Corradi <davide.corradi.dev@gmail.com>
2020-07-21 00:07:04 +0200

    Deref and unit tests for Instance

--- src/hal/instance.rs
+++ src/hal/instance.rs
@@ -10,14 +10,47 @@ pub struct Instance
 
 impl Instance
 {
+  pub const ENGINE_VERSION: u32 = 1;
+  pub const ENGINE_NAME: &'static str = "Red Ape Engine";
+
   pub fn new() -> Self
   {
     use hal::Instance;
-
-    const VERSION: u32 = 1;
-    const NAME: &'static str = "Red Ape Engine";
-    let value =
-      <Backend as hal::Backend>::Instance::create(NAME, VERSION).unwrap();
+    let value = <Backend as hal::Backend>::Instance::create(
+      Self::ENGINE_NAME,
+      Self::ENGINE_VERSION,
+    )
+    .unwrap();
     Self { value }
+  }
+}
+
+impl std::ops::Deref for Instance
+{
+  type Target = <Backend as hal::Backend>::Instance;
+
+  fn deref(&self) -> &Self::Target
+  {
+    &self.value
+  }
+}
+
+impl std::ops::DerefMut for Instance
+{
+  fn deref_mut(&mut self) -> &mut Self::Target
+  {
+    &mut self.value
+  }
+}
+
+#[cfg(test)]
+mod tests
+{
+  use super::*;
+
+  #[test]
+  fn instance_creation()
+  {
+    let _instance = Instance::new();
   }
 }
