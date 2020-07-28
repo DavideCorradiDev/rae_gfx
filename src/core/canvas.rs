3ce4bd05f679a4e28c60ee46996c50d99a213c40
Davide Corradi <davide.corradi.dev@gmail.com>
2020-07-28 23:18:46 +0200

    Canvas

---
+++ src/core/canvas.rs
@@ -0,0 +1,7 @@
+extern crate gfx_hal as hal;
+
+pub use hal::window::Extent2D as CanvasSize;
+
+pub trait Canvas
+{
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -4,5 +4,8 @@ pub use texture_format::TextureFormat;
 mod instance;
 pub use instance::{Instance, InstanceCreationError};
 
+mod canvas;
+pub use canvas::{Canvas, CanvasSize};
+
 mod canvas_window;
 pub use canvas_window::CanvasWindow;
