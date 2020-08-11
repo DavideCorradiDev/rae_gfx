757091ad276a046f1d748272c6075a8a72ec6318
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-11 22:53:07 +0200

    Separated functions

--- src/core/spirv.rs
+++ src/core/spirv.rs
@@ -1,39 +1,39 @@
 extern crate shaderc;
 
-pub fn compile_shaders_in_directory(
-    input_dir: std::path::PathBuf,
-    mut output_dir: std::path::PathBuf,
-) {
+pub fn compile_shader_file(in_path: std::path::PathBuf, out_path: std::path::PathBuf) {
+    let shader_kind = match in_path.extension() {
+        Some(extension) => match extension.to_str().unwrap() {
+            "vert" => shaderc::ShaderKind::Vertex,
+            "frag" => shaderc::ShaderKind::Fragment,
+            _ => panic!("Invalid extension!"),
+        },
+        _ => panic!("Invalid extension!"),
+    };
+    let source = std::fs::read_to_string(&in_path).unwrap();
     let mut compiler = shaderc::Compiler::new().unwrap();
+    let compiled_file = compiler
+        .compile_into_spirv(
+            &source,
+            shader_kind,
+            in_path.to_str().unwrap(),
+            "main",
+            None,
+        )
+        .unwrap();
+    std::fs::write(&out_path, compiled_file.as_binary_u8()).unwrap();
+}
 
+pub fn compile_shader_files(input_dir: std::path::PathBuf, output_dir: std::path::PathBuf) {
     std::fs::create_dir_all(output_dir.clone()).unwrap();
     for input_subdir in std::fs::read_dir(input_dir).unwrap() {
         let input_subdir = input_subdir.unwrap();
         if input_subdir.file_type().unwrap().is_file() {
             let input_path = input_subdir.path();
-            let shader_kind = input_path.extension().and_then(|extension| {
-                match extension.to_string_lossy().as_ref() {
-                    "vert" => Some(shaderc::ShaderKind::Vertex),
-                    "frag" => Some(shaderc::ShaderKind::Fragment),
-                    _ => None,
-                }
-            });
-            if let Some(shader_kind) = shader_kind {
-                let source = std::fs::read_to_string(&input_path).unwrap();
-                let compiled_file = compiler
-                    .compile_into_spirv(
-                        &source,
-                        shader_kind,
-                        input_path.to_str().unwrap(),
-                        "main",
-                        None,
-                    )
-                    .unwrap();
-                let output_filename = input_path.file_name().unwrap().to_str().unwrap();
-                output_dir.set_file_name(output_filename);
-                output_dir.set_extension("spv");
-                std::fs::write(&output_dir, compiled_file.as_binary_u8()).unwrap();
-            }
+            let output_filename = input_path.file_name().unwrap().to_str().unwrap();
+            let mut output_path = std::path::PathBuf::from(output_dir.clone());
+            output_path.set_file_name(output_filename);
+            output_path.set_extension("spv");
+            compile_shader_file(input_path, output_path);
         }
     }
 }
