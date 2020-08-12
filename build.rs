9e2ea87aa1063d1c6a676151ce9aa18fa6c8e735
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-12 23:23:17 +0200

    Modified shader compilation function

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 extern crate rae_shader;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let in_path = std::path::PathBuf::from("src/shaders/glsl");
-    let out_path = std::path::PathBuf::from("src/shaders/spirv");
+    let in_dir = std::path::PathBuf::from("src/shaders/glsl");
+    let out_dir = std::path::PathBuf::from("src/shaders/gen/spirv");
     println!(
         "cargo:rerun-if-changed={}/**",
-        in_path.to_str().unwrap_or("")
+        in_dir.to_str().unwrap_or("")
     );
-    rae_shader::compile_shaders_into_spirv(in_path, out_path)?;
+    rae_shader::compile_shaders_into_spirv(in_dir, out_dir)?;
     Ok(())
 }
