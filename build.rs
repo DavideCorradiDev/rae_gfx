a1baee29cf9dfdb2d436516c9829cc9003a6a874
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-12 22:22:49 +0200

    Remove spv file

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 extern crate rae_shader;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let in_path = std::path::PathBuf::from("src/shaders/glsl/");
-    let out_path = std::path::PathBuf::from("src/shaders/spirv/");
+    let in_path = std::path::PathBuf::from("src/shaders/glsl");
+    let out_path = std::path::PathBuf::from("src/shaders/spirv");
     println!(
-        "cargo:rerun-if-changed={}**",
+        "cargo:rerun-if-changed={}/**",
         in_path.to_str().unwrap_or("")
     );
     rae_shader::compile_shaders_into_spirv(in_path, out_path)?;
