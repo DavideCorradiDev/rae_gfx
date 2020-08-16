7a3ea41b8f93446b10b6a0319e24f2a8ee6d351e
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-16 10:32:05 +0200

    Added pipeline fields

--- src/core/pipeline.rs
+++ src/core/pipeline.rs
@@ -1,5 +1,9 @@
 use crate::halw;
 
+use std::{cell::RefCell, rc::Rc};
+
+use super::Canvas;
+
 pub trait PipelineConfig {
     type Vertex;
     type Constants;
@@ -8,4 +12,9 @@ pub trait PipelineConfig {
     fn fragment_shader_module(&self) -> &[u32];
 }
 
-pub struct Pipeline<Config: PipelineConfig> {}
+pub struct Pipeline<Config: PipelineConfig> {
+    canvas: Rc<RefCell<dyn Canvas>>,
+    layout: halw::PipelineLayout,
+    pipeline: halw::GraphicsPipeline,
+    _p: std::marker::PhantomData<Config>,
+}
