62e1203091d34fb3af66d8779b1dfc51bc937854
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-29 11:16:57 +0200

    Add example

---
+++ examples/triangles.rs
@@ -0,0 +1,109 @@
+use rae_app::{
+    application::Application,
+    event::{mouse, ControlFlow, DeviceId, EventHandler, EventLoop},
+    window,
+    window::{Window, WindowBuilder, WindowId},
+};
+
+use rae_gfx::wgpu::core::{Device, DeviceConfig, DeviceCreationError};
+
+type ApplicationEvent = ();
+
+#[derive(Debug)]
+enum ApplicationError {
+    WindowCreationFailed(window::OsError),
+    DeviceCreationFailed(DeviceCreationError),
+}
+
+impl std::fmt::Display for ApplicationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ApplicationError::WindowCreationFailed(e) => {
+                write!(f, "Window creation failed ({})", e)
+            }
+            ApplicationError::DeviceCreationFailed(e) => {
+                write!(f, "Device creation failed ({})", e)
+            }
+        }
+    }
+}
+
+impl std::error::Error for ApplicationError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ApplicationError::WindowCreationFailed(e) => Some(e),
+            ApplicationError::DeviceCreationFailed(e) => Some(e),
+        }
+    }
+}
+
+impl From<window::OsError> for ApplicationError {
+    fn from(e: window::OsError) -> Self {
+        ApplicationError::WindowCreationFailed(e)
+    }
+}
+
+impl From<DeviceCreationError> for ApplicationError {
+    fn from(e: DeviceCreationError) -> Self {
+        ApplicationError::DeviceCreationFailed(e)
+    }
+}
+
+#[derive(Debug)]
+struct ApplicationImpl {
+    window: Window,
+    device: Device,
+}
+
+impl EventHandler<ApplicationError, ApplicationEvent> for ApplicationImpl {
+    type Error = ApplicationError;
+    type CustomEvent = ApplicationEvent;
+
+    fn new(event_loop: &EventLoop<Self::CustomEvent>) -> Result<Self, Self::Error> {
+        let window = WindowBuilder::new()
+            .with_inner_size(window::Size::Physical(window::PhysicalSize {
+                width: 800,
+                height: 800,
+            }))
+            .build(event_loop)?;
+        let device =
+            futures::executor::block_on(Device::new(&DeviceConfig::high_performance(), None))?;
+        Ok(Self { window, device })
+    }
+
+    fn on_resized(
+        &mut self,
+        _wid: WindowId,
+        _size: window::PhysicalSize<u32>,
+    ) -> Result<ControlFlow, Self::Error> {
+        Ok(ControlFlow::Continue)
+    }
+
+    fn on_cursor_moved(
+        &mut self,
+        _wid: WindowId,
+        _device_id: DeviceId,
+        _position: window::PhysicalPosition<f64>,
+    ) -> Result<ControlFlow, Self::Error> {
+        Ok(ControlFlow::Continue)
+    }
+
+    fn on_mouse_button_released(
+        &mut self,
+        _wid: WindowId,
+        _device_id: DeviceId,
+        _button: mouse::Button,
+    ) -> Result<ControlFlow, Self::Error> {
+        Ok(ControlFlow::Continue)
+    }
+
+    fn on_variable_update(&mut self, _dt: std::time::Duration) -> Result<ControlFlow, Self::Error> {
+        Ok(ControlFlow::Continue)
+    }
+}
+
+fn main() {
+    const FIXED_FRAMERATE: u64 = 30;
+    const VARIABLE_FRAMERATE_CAP: u64 = 60;
+    Application::<ApplicationImpl, _, _>::new(FIXED_FRAMERATE, Some(VARIABLE_FRAMERATE_CAP)).run();
+}
