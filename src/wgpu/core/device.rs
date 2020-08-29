3d84af03d400203b03bb21e51e92c2d5a318b1c8
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-29 09:34:43 +0200

    Device and Queue creation

--- src/wgpu/core/device.rs
+++ src/wgpu/core/device.rs
@@ -1,23 +1,27 @@
-pub use wgpu::{BackendBit, PowerPreference};
+pub use wgpu::{BackendBit as Backend, Features, Limits, PowerPreference};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct DeviceConfig<'a> {
-    pub backend: BackendBit,
+    pub backend: Backend,
     pub power_preference: PowerPreference,
     pub compatible_surface: Option<&'a wgpu::Surface>,
+    pub required_features: Features,
+    pub optional_features: Features,
+    pub required_limits: Limits,
 }
 
 #[derive(Debug)]
 pub struct Device {
     instance: wgpu::Instance,
     adapter: wgpu::Adapter,
-    // device: wgpu::Device,
-    // queue: wgpu::Queue,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
 }
 
 impl Device {
     pub async fn new(config: &DeviceConfig<'_>) -> Result<Self, DeviceCreationError> {
         let instance = wgpu::Instance::new(config.backend);
+
         let adapter = match instance
             .request_adapter(&wgpu::RequestAdapterOptions {
                 power_preference: config.power_preference,
@@ -28,36 +32,63 @@ impl Device {
             Some(v) => v,
             None => return Err(DeviceCreationError::NoSuitableAdapter),
         };
-        Ok(Self { instance, adapter })
+
+        let available_features = adapter.features();
+        // TODO: write what features are missing in the error.
+        if !available_features.contains(config.required_features) {
+            return Err(DeviceCreationError::NoSuitableAdapter);
+        }
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: (config.optional_features & available_features)
+                        | config.required_features,
+                    limits: config.required_limits.clone(),
+                    shader_validation: true,
+                },
+                None,
+            )
+            .await?;
+
+        Ok(Self {
+            instance,
+            adapter,
+            device,
+            queue,
+        })
     }
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum DeviceCreationError {
-    UnsupportedBackend,
     NoSuitableAdapter,
-    NoSuitableQueueFamily,
-    SurfaceCreationFailed,
-    DeviceCreationFailed,
+    DeviceRequestFailed(wgpu::RequestDeviceError),
 }
 
 impl std::fmt::Display for DeviceCreationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            DeviceCreationError::UnsupportedBackend => write!(f, "Unsupported backend"),
             DeviceCreationError::NoSuitableAdapter => write!(f, "No suitable adapter"),
-            DeviceCreationError::NoSuitableQueueFamily => write!(f, "No suitable queue family"),
-            DeviceCreationError::SurfaceCreationFailed => {
-                write!(f, "Window surface creation failed")
+            DeviceCreationError::DeviceRequestFailed(e) => {
+                write!(f, "Device request failed ({})", e)
             }
-            DeviceCreationError::DeviceCreationFailed => write!(f, "Device creation failed"),
         }
     }
 }
 
 impl std::error::Error for DeviceCreationError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        None
+        match self {
+            DeviceCreationError::DeviceRequestFailed(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl From<wgpu::RequestDeviceError> for DeviceCreationError {
+    fn from(e: wgpu::RequestDeviceError) -> Self {
+        DeviceCreationError::DeviceRequestFailed(e)
     }
 }
 
@@ -68,9 +99,12 @@ mod tests {
     #[test]
     fn creation() {
         let _device = futures::executor::block_on(Device::new(&DeviceConfig {
-            backend: BackendBit::PRIMARY,
+            backend: Backend::PRIMARY,
             power_preference: PowerPreference::Default,
             compatible_surface: None,
+            required_features: Features::default(),
+            optional_features: Features::empty(),
+            required_limits: Limits::default(),
         }))
         .unwrap();
     }
