f0ff82cd6ec48397516ccd90f61f771792c01070
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-31 20:29:24 +0200

    Build render frame from canvas

--- src/core/render_frame.rs
+++ src/core/render_frame.rs
@@ -1,127 +1,93 @@
-use std::{
-    cell::RefCell,
-    ops::{Deref, DerefMut},
-    pin::Pin,
-    ptr::NonNull,
-};
+use std::ops::{Deref, DerefMut};
 
 use super::{
-    Color, CommandEncoder, CommandEncoderDescriptor, Instance, LoadOp, Operations, RenderPass,
-    RenderPassColorAttachmentDescriptor, RenderPassDescriptor, SwapChainFrame, TextureView,
+    Canvas, Color, CommandEncoder, CommandEncoderDescriptor, Instance, LoadOp, Operations,
+    RenderPass, RenderPassColorAttachmentDescriptor, RenderPassDescriptor, SwapChainError,
+    SwapChainTexture, TextureView,
 };
 
 #[derive(Debug)]
 pub struct RenderFrame<'a> {
-    render_pass: Option<RenderPass<'a>>,
+    render_pass: RenderPass<'a>,
     command_encoder: Box<CommandEncoder>,
-    attachment: Box<TextureView>,
-    resolve_target: Option<Box<TextureView>>,
+    swap_chain_texture: Option<Box<SwapChainTexture>>,
+    texture_view: Option<Box<TextureView>>,
 }
 
+// TODO: specify clear to color operations.
+
 impl<'a> RenderFrame<'a> {
-    pub fn new(
+    pub fn from_canvas<CanvasType: Canvas>(
         instance: &Instance,
-        attachment: TextureView,
-        resolve_target: Option<TextureView>,
+        canvas: &mut CanvasType,
+    ) -> Result<Self, SwapChainError> {
+        let frame = canvas.get_current_frame()?;
+        Ok(Self::from_texture_views(instance, Some(frame.output), None))
+    }
+
+    pub fn from_texture_views(
+        instance: &Instance,
+        swap_chain_texture: Option<SwapChainTexture>,
+        texture_view: Option<TextureView>,
     ) -> Self {
-        let command_encoder =
+        let mut command_encoder =
             Box::new(instance.create_command_encoder(&CommandEncoderDescriptor::default()));
-        let attachment = Box::new(attachment);
-        let resolve_target = match resolve_target {
+
+        let swap_chain_texture = match swap_chain_texture {
+            Some(v) => Some(Box::new(v)),
+            None => None,
+        };
+        let texture_view = match texture_view {
             Some(v) => Some(Box::new(v)),
             None => None,
         };
 
-        let mut render_frame = Self {
-            render_pass: None,
-            command_encoder,
-            attachment,
-            resolve_target,
-        };
-        unsafe {
-            let command_encoder_ptr =
-                render_frame.command_encoder.deref_mut() as *mut CommandEncoder;
-            let attachment_ptr = render_frame.attachment.deref() as *const TextureView;
-            let resolve_target_ptr = match &render_frame.resolve_target {
-                Some(v) => Some(&*(v.deref() as *const TextureView)),
-                None => None,
+        // The render pass will hold a reference to the command encoder and the texture views.
+        // Since it will store them inside a Box, on the heap, their addresses should be stable.
+        // Additional borrowing of the resources is prevented by keeping these resources hidden inside the struct.
+        let render_pass = unsafe {
+            let command_encoder_ref = &mut *(command_encoder.deref_mut() as *mut CommandEncoder);
+
+            let (attachment_ref, resolve_target_ref) = match &texture_view {
+                Some(tv) => match &swap_chain_texture {
+                    Some(sct) => (
+                        &*(tv.deref() as *const TextureView),
+                        Some(&*(&sct.view as *const TextureView)),
+                    ),
+                    None => (&*(tv.deref() as *const TextureView), None),
+                },
+                None => match &swap_chain_texture {
+                    Some(sct) => (&*(&sct.view as *const TextureView), None),
+                    None => panic!("No main attachment specified when creating a render frame."),
+                },
             };
-            let render_pass = (*command_encoder_ptr).begin_render_pass(&RenderPassDescriptor {
+
+            command_encoder_ref.begin_render_pass(&RenderPassDescriptor {
                 color_attachments: &[RenderPassColorAttachmentDescriptor {
-                    attachment: &*attachment_ptr,
-                    resolve_target: resolve_target_ptr,
+                    attachment: attachment_ref,
+                    resolve_target: resolve_target_ref,
                     ops: Operations {
                         load: LoadOp::Clear(Color::BLACK),
                         store: true,
                     },
                 }],
                 depth_stencil_attachment: None,
-            });
-            render_frame.render_pass = Some(render_pass);
+            })
+        };
+
+        Self {
+            render_pass,
+            command_encoder,
+            swap_chain_texture,
+            texture_view,
         }
-        render_frame
+    }
+
+    pub fn render_pass(&self) -> &RenderPass {
+        &self.render_pass
+    }
+
+    pub fn render_pass_mut(&mut self) -> &'a mut RenderPass {
+        &mut self.render_pass
     }
 }
-
-// TODO: specify clear operations.
-
-// impl<'a> RenderFrame<'a> {
-//     pub fn new(
-//         instance: &Instance,
-//         attachment: TextureView,
-//         // resolve_target: Option<Box<TextureView>>,
-//     ) -> Pin<Box<Self>> {
-//         let mut command_encoder =
-//             instance.create_command_encoder(&CommandEncoderDescriptor::default());
-//
-//         let mut render_frame = Box::pin(Self {
-//             render_pass: None,
-//             command_encoder,
-//             attachment,
-//         });
-//
-//         unsafe {
-//             let render_pass = Some(render_frame.command_encoder.begin_render_pass(
-//                 &RenderPassDescriptor {
-//                     color_attachments: &[RenderPassColorAttachmentDescriptor {
-//                         attachment: &render_frame.attachment,
-//                         resolve_target: None,
-//                         ops: Operations {
-//                             load: LoadOp::Clear(Color::BLACK),
-//                             store: true,
-//                         },
-//                     }],
-//                     depth_stencil_attachment: None,
-//                 },
-//             ));
-//
-//             let mut_ref = Pin::as_mut(&mut render_frame);
-//             Pin::get_unchecked_mut(mut_ref).render_pass = render_pass;
-//         }
-//
-//         render_frame
-//
-//         // let attachment = Box::pin(attachment);
-//         // let render_pass = {
-//         //     command_encoder.begin_render_pass(&RenderPassDescriptor {
-//         //         color_attachments: &[RenderPassColorAttachmentDescriptor {
-//         //             attachment: &attachment,
-//         //             resolve_target: None,
-//         //             ops: Operations {
-//         //                 load: LoadOp::Clear(Color::BLACK),
-//         //                 store: true,
-//         //             },
-//         //         }],
-//         //         depth_stencil_attachment: None,
-//         //     })
-//         // };
-//
-//         // unsafe {
-//         //     Self {
-//         //         render_pass,
-//         //         command_encoder,
-//         //         attachment,
-//         //     }
-//         // }
-//     }
-// }
