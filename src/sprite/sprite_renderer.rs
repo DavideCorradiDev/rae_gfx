0b0caa50832ccf3d783e00d753ce40df125c8e94
Davide Corradi <davide.corradi.dev@gmail.com>
2020-09-09 23:33:01 +0200

    Push constants

--- src/sprite/sprite_renderer.rs
+++ src/sprite/sprite_renderer.rs
@@ -33,3 +33,41 @@ unsafe impl bytemuck::Pod for Vertex {}
 pub type Index = core::Index;
 
 pub type Mesh = core::IndexedMesh<Vertex>;
+
+#[derive(Debug, PartialEq, Clone, Copy, serde::Serialize, serde::Deserialize)]
+pub struct PushConstants {
+    transform: geometry3::HomogeneousMatrix<f32>,
+    color: [f32; 4],
+}
+
+impl PushConstants {
+    pub fn new(transform: &geometry2::Transform<f32>, color: core::Color) -> Self {
+        Self {
+            transform: transform.to_homogeneous3(),
+            color: [
+                color.r as f32,
+                color.g as f32,
+                color.b as f32,
+                color.a as f32,
+            ],
+        }
+    }
+
+    fn as_slice(&self) -> &[u32] {
+        let pc: *const PushConstants = self;
+        let pc: *const u8 = pc as *const u8;
+        let data = unsafe { std::slice::from_raw_parts(pc, std::mem::size_of::<PushConstants>()) };
+        bytemuck::cast_slice(&data)
+    }
+}
+
+unsafe impl bytemuck::Zeroable for PushConstants {
+    fn zeroed() -> Self {
+        Self {
+            transform: geometry3::HomogeneousMatrix::zero(),
+            color: [0., 0., 0., 0.],
+        }
+    }
+}
+
+unsafe impl bytemuck::Pod for PushConstants {}
