01943d94dde97fb63cef86dc3478f2ca9b82191a
Davide Corradi <davide.corradi.dev@gmail.com>
2020-09-09 23:35:16 +0200

    sprite renderer

--- src/sprite/sprite_renderer.rs
+++ src/sprite/sprite_renderer.rs
@@ -5,7 +5,7 @@ use num_traits::Zero;
 
 use rae_math::{conversion::ToHomogeneous3, geometry2, geometry3};
 
-use crate::core;
+use crate::{core, core::IndexedMeshRenderer};
 
 #[derive(Debug, PartialEq, Clone, Copy, serde::Serialize, serde::Deserialize)]
 pub struct Vertex {
@@ -71,3 +71,159 @@ unsafe impl bytemuck::Zeroable for PushConstants {
 }
 
 unsafe impl bytemuck::Pod for PushConstants {}
+
+#[derive(Debug, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
+pub struct RenderPipelineDescriptor {
+    pub color_blend: core::BlendDescriptor,
+    pub alpha_blend: core::BlendDescriptor,
+    pub write_mask: core::ColorWrite,
+    pub color_buffer_format: core::ColorBufferFormat,
+    pub sample_count: core::SampleCount,
+}
+
+impl Default for RenderPipelineDescriptor {
+    fn default() -> Self {
+        Self {
+            color_blend: core::BlendDescriptor {
+                src_factor: core::BlendFactor::SrcAlpha,
+                dst_factor: core::BlendFactor::OneMinusSrcAlpha,
+                operation: core::BlendOperation::Add,
+            },
+            alpha_blend: core::BlendDescriptor {
+                src_factor: core::BlendFactor::One,
+                dst_factor: core::BlendFactor::One,
+                operation: core::BlendOperation::Max,
+            },
+            write_mask: core::ColorWrite::ALL,
+            color_buffer_format: core::ColorBufferFormat::default(),
+            sample_count: 1,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RenderPipeline {
+    pipeline: core::RenderPipeline,
+    sample_count: core::SampleCount,
+    color_buffer_format: core::ColorBufferFormat,
+}
+
+impl RenderPipeline {
+    pub fn new(instance: &core::Instance, desc: &RenderPipelineDescriptor) -> Self {
+        let pipeline_layout = core::PipelineLayout::new(
+            &instance,
+            &core::PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[],
+                push_constant_ranges: &[core::PushConstantRange {
+                    stages: core::ShaderStage::VERTEX,
+                    range: 0..std::mem::size_of::<PushConstants>() as u32,
+                }],
+            },
+        );
+        let vs_module = core::ShaderModule::new(
+            &instance,
+            core::include_spirv!("shaders/gen/spirv/sprite.vert.spv"),
+        );
+        let fs_module = core::ShaderModule::new(
+            &instance,
+            core::include_spirv!("shaders/gen/spirv/sprite.frag.spv"),
+        );
+        let pipeline = core::RenderPipeline::new(
+            &instance,
+            &core::RenderPipelineDescriptor {
+                label: None,
+                layout: Some(&pipeline_layout),
+                vertex_stage: core::ProgrammableStageDescriptor {
+                    module: &vs_module,
+                    entry_point: "main",
+                },
+                fragment_stage: Some(core::ProgrammableStageDescriptor {
+                    module: &fs_module,
+                    entry_point: "main",
+                }),
+                rasterization_state: Some(core::RasterizationStateDescriptor {
+                    front_face: core::FrontFace::Ccw,
+                    cull_mode: core::CullMode::Back,
+                    ..Default::default()
+                }),
+                primitive_topology: core::PrimitiveTopology::TriangleList,
+                color_states: &[core::ColorStateDescriptor {
+                    format: core::TextureFormat::from(desc.color_buffer_format),
+                    color_blend: desc.color_blend.clone(),
+                    alpha_blend: desc.alpha_blend.clone(),
+                    write_mask: desc.write_mask,
+                }],
+                depth_stencil_state: None,
+                vertex_state: core::VertexStateDescriptor {
+                    index_format: core::IndexFormat::Uint16,
+                    vertex_buffers: &[core::VertexBufferDescriptor {
+                        stride: std::mem::size_of::<Vertex>() as core::BufferAddress,
+                        step_mode: core::InputStepMode::Vertex,
+                        attributes: &[core::VertexAttributeDescriptor {
+                            format: core::VertexFormat::Float2,
+                            offset: 0,
+                            shader_location: 0,
+                        }],
+                    }],
+                },
+                sample_count: desc.sample_count,
+                sample_mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+        );
+        Self {
+            pipeline,
+            sample_count: desc.sample_count,
+            color_buffer_format: desc.color_buffer_format,
+        }
+    }
+
+    pub fn render_pass_requirements(&self) -> core::RenderPassRequirements {
+        core::RenderPassRequirements {
+            sample_count: self.sample_count,
+            color_buffer_formats: vec![self.color_buffer_format],
+            depth_stencil_buffer_format: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct DrawMesh<'a> {
+    pub mesh: &'a Mesh,
+    pub index_range: Range<u32>,
+    pub constants: &'a PushConstants,
+}
+
+pub trait Renderer<'a> {
+    fn draw_shape2<It>(&mut self, pipeline: &'a RenderPipeline, draw_mesh_commands: It)
+    where
+        It: IntoIterator,
+        It::Item: Borrow<DrawMesh<'a>>;
+}
+
+impl<'a> Renderer<'a> for core::RenderPass<'a> {
+    fn draw_shape2<It>(&mut self, pipeline: &'a RenderPipeline, draw_mesh_commands: It)
+    where
+        It: IntoIterator,
+        It::Item: Borrow<DrawMesh<'a>>,
+    {
+        self.set_pipeline(&pipeline.pipeline);
+        for draw_mesh in draw_mesh_commands.into_iter() {
+            let draw_mesh = draw_mesh.borrow();
+            self.set_push_constants(core::ShaderStage::VERTEX, 0, draw_mesh.constants.as_slice());
+            self.draw_indexed_mesh(draw_mesh.mesh, &draw_mesh.index_range);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creation() {
+        let instance = core::Instance::new(&core::InstanceDescriptor::default()).unwrap();
+        let _pipeline = RenderPipeline::new(&instance, &RenderPipelineDescriptor::default());
+    }
+}
