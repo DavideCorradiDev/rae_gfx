1f8271fb25bb2180a63b654b8b808ddf63ac8003
Davide Corradi <davide.corradi.dev@gmail.com>
2020-09-11 21:41:32 +0200

    Setup new example app

---
+++ examples/sprite.rs
@@ -0,0 +1,178 @@
+use rae_app::{
+    application::Application,
+    event::{ControlFlow, EventHandler, EventLoop},
+    window,
+    window::{WindowBuilder, WindowId},
+};
+
+use rae_math::geometry2::{OrthographicProjection, Projective};
+
+use rae_gfx::{
+    core::{
+        Canvas, CanvasWindow, CanvasWindowDescriptor, CommandSequence, Instance,
+        InstanceCreationError, InstanceDescriptor, RenderPassOperations, SampleCount,
+        SwapChainError,
+    },
+    sprite,
+    sprite::Renderer as Shape2Renderer,
+};
+
+#[derive(Debug)]
+struct ApplicationImpl {
+    window: CanvasWindow,
+    instance: Instance,
+    pipeline: sprite::RenderPipeline,
+    projection_transform: Projective<f32>,
+}
+
+impl ApplicationImpl {
+    const SAMPLE_COUNT: SampleCount = 8;
+}
+
+impl EventHandler<ApplicationError, ApplicationEvent> for ApplicationImpl {
+    type Error = ApplicationError;
+    type CustomEvent = ApplicationEvent;
+
+    fn new(event_loop: &EventLoop<Self::CustomEvent>) -> Result<Self, Self::Error> {
+        let window = WindowBuilder::new()
+            .with_inner_size(window::Size::Physical(window::PhysicalSize {
+                width: 800,
+                height: 800,
+            }))
+            .build(event_loop)?;
+        let (window, instance) = unsafe {
+            let (instance, surface) = Instance::new_with_compatible_window(
+                &InstanceDescriptor::high_performance(),
+                &window,
+            )?;
+            let window = CanvasWindow::from_window_and_surface(
+                &instance,
+                window,
+                surface,
+                &CanvasWindowDescriptor {
+                    sample_count: Self::SAMPLE_COUNT,
+                    ..CanvasWindowDescriptor::default()
+                },
+            );
+            (window, instance)
+        };
+
+        let pipeline = sprite::RenderPipeline::new(
+            &instance,
+            &sprite::RenderPipelineDescriptor {
+                sample_count: Self::SAMPLE_COUNT,
+                ..sprite::RenderPipelineDescriptor::default()
+            },
+        );
+
+        let window_size = window.inner_size();
+
+        // This matrix will flip the y axis, so that screen coordinates follow mouse
+        // coordinates.
+        let projection_transform = OrthographicProjection::new(
+            0.,
+            window_size.width as f32,
+            window_size.height as f32,
+            0.,
+        )
+        .to_projective();
+
+        Ok(Self {
+            window,
+            instance,
+            pipeline,
+            projection_transform,
+        })
+    }
+
+    fn on_resized(
+        &mut self,
+        wid: WindowId,
+        size: window::PhysicalSize<u32>,
+    ) -> Result<ControlFlow, Self::Error> {
+        if wid == self.window.id() {
+            self.window.update_buffers(&self.instance);
+            self.projection_transform = OrthographicProjection::new(
+                0.,
+                1f32.max(size.width as f32),
+                1f32.max(size.height as f32),
+                0.,
+            )
+            .to_projective();
+        }
+        Ok(ControlFlow::Continue)
+    }
+
+    fn on_variable_update(&mut self, dt: std::time::Duration) -> Result<ControlFlow, Self::Error> {
+        let frame = self.window.current_frame()?;
+        let mut cmd_sequence = CommandSequence::new(&self.instance);
+        {
+            let mut _rpass = cmd_sequence.begin_render_pass(
+                &frame,
+                &self.pipeline.render_pass_requirements(),
+                &RenderPassOperations::default(),
+            );
+        }
+        cmd_sequence.submit(&self.instance);
+        Ok(ControlFlow::Continue)
+    }
+}
+
+type ApplicationEvent = ();
+
+#[derive(Debug)]
+enum ApplicationError {
+    WindowCreationFailed(window::OsError),
+    InstanceCreationFailed(InstanceCreationError),
+    RenderFrameCreationFailed(SwapChainError),
+}
+
+impl std::fmt::Display for ApplicationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ApplicationError::WindowCreationFailed(e) => {
+                write!(f, "Window creation failed ({})", e)
+            }
+            ApplicationError::InstanceCreationFailed(e) => {
+                write!(f, "Instance creation failed ({})", e)
+            }
+            ApplicationError::RenderFrameCreationFailed(e) => {
+                write!(f, "Render frame creation failed ({})", e)
+            }
+        }
+    }
+}
+
+impl std::error::Error for ApplicationError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ApplicationError::WindowCreationFailed(e) => Some(e),
+            ApplicationError::InstanceCreationFailed(e) => Some(e),
+            ApplicationError::RenderFrameCreationFailed(e) => Some(e),
+        }
+    }
+}
+
+impl From<window::OsError> for ApplicationError {
+    fn from(e: window::OsError) -> Self {
+        ApplicationError::WindowCreationFailed(e)
+    }
+}
+
+impl From<InstanceCreationError> for ApplicationError {
+    fn from(e: InstanceCreationError) -> Self {
+        ApplicationError::InstanceCreationFailed(e)
+    }
+}
+
+impl From<SwapChainError> for ApplicationError {
+    fn from(e: SwapChainError) -> Self {
+        ApplicationError::RenderFrameCreationFailed(e)
+    }
+}
+
+fn main() {
+    const FIXED_FRAMERATE: u64 = 30;
+    const VARIABLE_FRAMERATE_CAP: u64 = 60;
+    Application::<ApplicationImpl, _, _>::new(FIXED_FRAMERATE, Some(VARIABLE_FRAMERATE_CAP)).run();
+}
